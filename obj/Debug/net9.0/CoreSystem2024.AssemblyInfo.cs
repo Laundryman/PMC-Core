5a10ad8fbf5a4112de29e0af409ad8409fbee02c
Laundryman <nickrenny@gmail.com>
2026-04-15 09:56:26 +0100

    Update submodules and assembly version info
    Updated PMC-Core to 7c79192 and marked PM-Planner as dirty. Synchronized AssemblyInformationalVersion and related cache files to reflect new submodule commit. Updated assembly reference cache due to dependency changes.

--- obj/Debug/net9.0/CoreSystem2024.AssemblyInfo.cs
+++ obj/Debug/net9.0/CoreSystem2024.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("...")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63c301a80bc7fe8d554b83fe798fc6f9e7ba68b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c79192fea86ca85e60180a5ff413b6a2c41edb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreSystemII")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreSystem2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
